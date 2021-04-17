6cb0a3eff2bd9cca8ae1b9825ef44e2fbc275253
ronyexchange0 <ronyexchange0@gmail.com>
2021-04-17 15:12:46 +0200

    fixed build

--- pages/blog.js
+++ pages/blog.js
@@ -1,27 +1,37 @@
-import { motion } from "framer-motion"
-import Loader from "../components/Loader"
-import Posts from "../components/Posts"
-import { getArticles } from "../utils"
+// import { motion } from "framer-motion"
+// import Loader from "../components/Loader"
+// import Posts from "../components/Posts"
+// import { getArticles } from "../utils"
 
-export const getStaticProps = async () => {
+// export const getStaticProps = async () => {
 
-    const posts = await getArticles(6)
+//     const posts = await getArticles(6)
 
-    return {
-        props: {posts},
-        revalidate: 10,
-    }
-}
+//     return {
+//         props: {posts},
+//         revalidate: 10,
+//     }
+// }
 
-const blogPage = ({posts}) => {
+// const blogPage = ({posts}) => {
+//     return (
+//         <main>
+//             <Loader/>
+//             <motion.div exit={{opacity: 0}}>
+//                 <Posts posts={posts} />
+//             </motion.div>
+//         </main>
+//     )
+// }
+
+// export default blogPage
+
+const Blog = () => {
     return (
-        <main>
-            <Loader/>
-            <motion.div exit={{opacity: 0}}>
-                <Posts posts={posts} />
-            </motion.div>
-        </main>
+        <div>
+            TRY LATER
+        </div>
     )
 }
 
-export default blogPage
+export default Blog
--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,126 +1,137 @@
-import ReactMarkdown from "react-markdown";
-import { useEffect } from "react";
-import { useInView } from 'react-intersection-observer';
-import { motion, useAnimation } from "framer-motion";
+// import ReactMarkdown from "react-markdown";
+// import { useEffect } from "react";
+// import { useInView } from 'react-intersection-observer';
+// import { motion, useAnimation } from "framer-motion";
 
-import Meta from "../../components/Meta";
-import HeadingLayout from "../../components/HeadingLayout";
-import Loader from "../../components/Loader";
-import { getArticle, getArticles } from "../../utils";
-import PostsList from "../../components/PostsList";
-import { fadeUp, textSlideUp } from "../../animations";
+// import Meta from "../../components/Meta";
+// import HeadingLayout from "../../components/HeadingLayout";
+// import Loader from "../../components/Loader";
+// import { getArticle, getArticles } from "../../utils";
+// import PostsList from "../../components/PostsList";
+// import { fadeUp, textSlideUp } from "../../animations";
 
-import styles from "../../styles/ServicePage.module.scss";
+// import styles from "../../styles/ServicePage.module.scss";
 
-const Post = ({post, morePosts}) => {
+// const Post = ({post, morePosts}) => {
 
-    const contentAnimation = useAnimation()
-    const [contentRef, contentInView] = useInView({
-        triggerOnce: true,
-        rootMargin: "-150px"
-    })
+//     const contentAnimation = useAnimation()
+//     const [contentRef, contentInView] = useInView({
+//         triggerOnce: true,
+//         rootMargin: "-150px"
+//     })
 
-    useEffect(() => {
-        if (contentInView) {
-            contentAnimation.start("animate")
-        }
-    }, [contentAnimation, contentInView])
+//     useEffect(() => {
+//         if (contentInView) {
+//             contentAnimation.start("animate")
+//         }
+//     }, [contentAnimation, contentInView])
 
 
-    const animation = useAnimation()
-    const [morePostsRef, inView] = useInView({
-        triggerOnce: true,
-        rootMargin: "-150px"
-    })
+//     const animation = useAnimation()
+//     const [morePostsRef, inView] = useInView({
+//         triggerOnce: true,
+//         rootMargin: "-150px"
+//     })
 
-    useEffect(() => {
-        if (inView) {
-            animation.start("animate")
-        }
-    }, [animation, inView])
+//     useEffect(() => {
+//         if (inView) {
+//             animation.start("animate")
+//         }
+//     }, [animation, inView])
 
-    const {title, image: {url}, content, description} = post
+//     const {title, image: {url}, content, description} = post
 
-    return (
-        <main className={styles.servicePage}>
-            <Meta
-            title={title}
-            description={description}
-            />
+//     return (
+//         <main className={styles.servicePage}>
+//             <Meta
+//             title={title}
+//             description={description}
+//             />
             
-            <Loader/>
+//             <Loader/>
 
-            <motion.div className="container" exit={{opacity: 0}} initial="initial" animate={animation}>
+//             <motion.div className="container" exit={{opacity: 0}} initial="initial" animate={animation}>
 
-                <HeadingLayout image={url} title={title}  style={{marginBottom: "60px"}}>
-                    {description}
-                </HeadingLayout>
-
-
-                <motion.div 
-                initial="initial" 
-                animate={contentAnimation} 
-                ref={contentRef} 
-                className="row justify-content-center"
-                >
-
-                    <motion.div variants={fadeUp(1)} className={`${styles.content} col-md-10 col-lg-8 text-center`}>
-                        <ReactMarkdown source={content} escapeHtml={false} />
-                    </motion.div>
-
-                </motion.div>
-
-                {
-                    morePosts.length > 0 ? (
-                        <div className={styles.morePosts} ref={morePostsRef} >
-
-                            <div className={styles.heading}>
-
-                                <div className={styles.textWrapper} style={{marginBottom: 0}}>
-                                    <motion.span variants={textSlideUp(.2)}>Recommended For You</motion.span>
-                                </div>
-
-                                <div className={styles.textWrapper}>
-                                    <motion.h2 variants={textSlideUp(.5)}>Featured Articles</motion.h2>
-                                </div>
-
-                            </div>
-
-                            <PostsList posts={morePosts} />
-
-                        </div>
-                    ) : ""
-                }
+//                 <HeadingLayout image={url} title={title}  style={{marginBottom: "60px"}}>
+//                     {description}
+//                 </HeadingLayout>
 
 
-            </motion.div>
+//                 <motion.div 
+//                 initial="initial" 
+//                 animate={contentAnimation} 
+//                 ref={contentRef} 
+//                 className="row justify-content-center"
+//                 >
 
-        </main>
-    )
-}
+//                     <motion.div variants={fadeUp(1)} className={`${styles.content} col-md-10 col-lg-8 text-center`}>
+//                         <ReactMarkdown source={content} escapeHtml={false} />
+//                     </motion.div>
 
-export default Post
+//                 </motion.div>
 
-export const getStaticPaths = async () => {
-    const posts = await getArticles()
+//                 {
+//                     morePosts.length > 0 ? (
+//                         <div className={styles.morePosts} ref={morePostsRef} >
+
+//                             <div className={styles.heading}>
+
+//                                 <div className={styles.textWrapper} style={{marginBottom: 0}}>
+//                                     <motion.span variants={textSlideUp(.2)}>Recommended For You</motion.span>
+//                                 </div>
+
+//                                 <div className={styles.textWrapper}>
+//                                     <motion.h2 variants={textSlideUp(.5)}>Featured Articles</motion.h2>
+//                                 </div>
+
+//                             </div>
+
+//                             <PostsList posts={morePosts} />
+
+//                         </div>
+//                     ) : ""
+//                 }
+
+
+//             </motion.div>
+
+//         </main>
+//     )
+// }
+
+// export default Post
+
+// export const getStaticPaths = async () => {
+//     const posts = await getArticles()
     
-    return {
-        paths: posts.map(post => ({
-            params: {
-                slug: post.slug,
-            }
-        })),
-        fallback: false
-    }
-}
-export const getStaticProps = async ({params}) => {
+//     return {
+//         paths: posts.map(post => ({
+//             params: {
+//                 slug: post.slug,
+//             }
+//         })),
+//         fallback: false
+//     }
+// }
+// export const getStaticProps = async ({params}) => {
     
    
 
-    const post = await getArticle(params.slug) 
-    const morePosts = await getArticles(2, post.category.slug, params.slug)
-    return {
-        props: {post, morePosts},
-        revalidate: 10,
-    }
+//     const post = await getArticle(params.slug) 
+//     const morePosts = await getArticles(2, post.category.slug, params.slug)
+//     return {
+//         props: {post, morePosts},
+//         revalidate: 10,
+//     }
+// }
+
+
+const PostPage = () => {
+    return (
+        <div>
+            TRY LATER
+        </div>
+    )
 }
+
+export default PostPage
